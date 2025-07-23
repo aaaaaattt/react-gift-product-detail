8a1c62c4715abffc60e3ae630977eb916ff5008e
aaaaaattt <choigur03@gamil.com>
2025-07-23 12:22:47 +0900

    feat: QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,19 @@ import { UserInfoProvider } from "@/context/UserInfoProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
       <ThemeProvider theme={theme}>
         <Layout>
           <UserInfoProvider>
             <NavigationBar />
-            <Outlet />
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
           </UserInfoProvider>
         </Layout>
       </ThemeProvider>
