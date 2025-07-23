df177d486a6b61df583aed1cb3f89f18595f7b18
aaaaaattt <choigur03@gamil.com>
2025-07-23 22:08:13 +0900

    fix: useEffect 로직 제거,  react Query 활용

--- src/components/GiftRankingBox.tsx
+++ src/components/GiftRankingBox.tsx
@@ -3,11 +3,9 @@ import { css } from "@emotion/react";
 import GiftObject from "./GiftObject";
 import { useTheme } from "@emotion/react";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import { ClipLoader } from "react-spinners";
 import { api } from "@/libs/axios";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 
 type ProductRanking = {
   id: number;
@@ -32,33 +30,20 @@ type GiftRankingProps = {
 
 const GiftRanking = ({ target, rankType }: GiftRankingProps) => {
   const theme = useTheme();
-  const [productRankingData, setProductRankingData] = useState<
-    ProductRanking[] | null
-  >(null);
-  const [isLoading, setIsLoading] = useState(true);
+
   const productsFiltered = `/products/ranking?targetType=${target}&rankType=${rankType}`;
   const navigate = useNavigate();
 
-  const { data, error, isError } = useQuery({
+  const {
+    data: productRankingData,
+    isError,
+    isLoading,
+  } = useQuery({
     queryKey: ["productRanking", target, rankType],
     queryFn: () => api.get(productsFiltered),
     enabled: !!target && !!rankType,
     select: (data) => data.data.data,
   });
-
-  useEffect(() => {
-    if (isError) {
-      if (axios.isAxiosError(error)) {
-        setIsLoading(false);
-        return;
-      }
-    }
-
-    if (data) {
-      setProductRankingData(data);
-      setIsLoading(false);
-    }
-  }, [target, rankType, data, error, isError]);
 
   return (
     <div css={giftRankingStyle(theme)}>
@@ -71,7 +56,7 @@ const GiftRanking = ({ target, rankType }: GiftRankingProps) => {
       {!isError && !isLoading && (
         <>
           {productRankingData &&
-            productRankingData.map((product) => (
+            productRankingData.map((product: ProductRanking) => (
               <GiftObject
                 key={product.id}
                 gift={product}
