2ec40d3d5f1ee725a76db3ce9b655304cffbc0bf
aaaaaattt <choigur03@gamil.com>
2025-07-27 13:39:44 +0900

    feat(layout): 레이아웃 좌우 영역 배경 색 변경

--- src/styles/Layout.tsx
+++ src/styles/Layout.tsx
@@ -1,8 +1,17 @@
 import { css } from "@emotion/react";
+import type { Theme } from "@emotion/react";
+import theme from "./theme";
+
+const outerWrapperStyle = (theme: Theme) => css`
+  background-color: ${theme.colors.gray.gray100};
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
 const layoutStyle = css`
   max-width: 720px;
-  margin: 0 auto;
+  background-color: white;
   padding: 0 16px;
 `;
 
@@ -10,8 +19,12 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Layout = (props: Props) => {
-  return <div css={layoutStyle}>{props.children}</div>;
+const Layout = ({ children }: Props) => {
+  return (
+    <div css={outerWrapperStyle(theme)}>
+      <div css={layoutStyle}>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
