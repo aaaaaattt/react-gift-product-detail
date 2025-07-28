da929e92145551c5d3d568db71ccb20244c41d2d
aaaaaattt <choigur03@gamil.com>
2025-07-28 19:03:45 +0900

    feat: suspense 출력 텍스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ErrorBoundary from "@/common/ErrorBoundary";
+import { Suspense } from "react";
 
 function App() {
   const queryClient = new QueryClient();
@@ -21,7 +22,9 @@ function App() {
             <NavigationBar />
             <QueryClientProvider client={queryClient}>
               <ErrorBoundary>
-                <Outlet />
+                <Suspense fallback={<div>로딩중...</div>}>
+                  <Outlet />
+                </Suspense>
               </ErrorBoundary>
             </QueryClientProvider>
           </UserInfoProvider>
